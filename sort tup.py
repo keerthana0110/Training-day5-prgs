93f245726d831d460493684904d0920f466e3821
Keerthana.T <48148816+keerthana0110@users.noreply.github.com>
2019-08-11 20:50:55 +0530

    Create sort tup.py

---
+++ sort tup.py
@@ -0,0 +1,15 @@
+from operator import itemgetter
+
+persons = []
+
+while True:
+	line = input("> ")
+	if not line:
+		break
+	persons.append(tuple(line.split(',')))
+
+persons = sorted(persons, key=itemgetter(0,1,2))
+
+for person in persons:
+	print (','.join(person))
+
